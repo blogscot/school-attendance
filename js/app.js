8a8b33aab2096c44262da391289b16181410a579
blogscot <iain.diamond@gmail.com>
2015-05-09 17:01:08 +0100

    Add local storage persistance

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ var students = [
   },
   {
     name: 'Edburg',
-    days: [0,0,0,0,1,0,0,1,0,0,1,0]
+    days: [0,0,0,0,0,1,0,1,0,0,1,0]
   },
   {
     name: 'Felix',
@@ -28,17 +28,20 @@ var students = [
   }
 ];
 
-var Day = function(isChecked) {
-  this.isChecked = ko.observable(isChecked);
+// Persist the attendance data using a unique local storage key
+var Day = function(isChecked, key) {
+  this.isChecked = ko.observable(isChecked, {persist: key});
 };
 
 var Student = function(student){
 
   this.days = [];
-  this.name = ko.observable(student.name);
+  this.name = student.name;
   
   for (var i = 0; i < student.days.length; i++) {
-    this.days.push(new Day(student.days[i] === 1));
+
+    // Combines the student name and day index to product the key
+    this.days.push(new Day(student.days[i] === 1, this.name+i));
   }
 
 };
