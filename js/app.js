c847654b2e2416a91b1684265e235dbf19f334e4
blogscot <iain.diamond@gmail.com>
2015-05-09 10:45:50 +0100

    corrected use of observableArray

--- js/app.js
+++ js/app.js
@@ -1,55 +1,58 @@
 /*global ko:false*/
 
 "use strict";
-var attendance = [0,0,0,0,1,0,0,1,0,0,0,0];
-
 var students = [
   {
     name: 'Andres',
-    days: attendance
+    days: [0,0,0,0,1,0,0,1,1,1,0,0]
   },
   {
     name: 'Brian',
-    days: attendance
+    days: [0,0,0,1,0,0,0,0,0,0,1,0]
   },
   {
     name: 'Cathrine',
-    days: attendance
+    days: [1,0,0,0,1,0,0,1,0,0,0,0]
   },
   {
     name: 'Derek',
-    days: attendance
+    days: [0,0,0,0,1,0,0,0,1,0,0,0]
   },
   {
     name: 'Edburg',
-    days: attendance
+    days: [0,0,0,0,1,0,0,1,0,0,1,0]
   },
   {
     name: 'Felix',
-    days: attendance
-  },
+    days: [0,0,1,0,1,0,0,0,0,0,0,0]
+  }
 ];
+
+var Day = function(isChecked) {
+  this.isChecked = ko.observable(isChecked);
+};
 
 var Student = function(student){
 
+  this.days = [];
   this.name = ko.observable(student.name);
-  this.days = ko.observableArray(student.days);
-};
+  
+  for (var i = 0; i < student.days.length; i++) {
+    this.days.push(new Day(student.days[i] === 1));
+  }
 
+};
 
 var ViewModel = function() {
   var self = this;
-  
-  var attendanceDays = [1,2,3,4,5,6,7,8,9,10,11,12];
-  this.attendance = ko.observableArray(attendanceDays);
 
+  this.attendance = [1,2,3,4,5,6,7,8,9,10,11,12];
   this.studentList = ko.observableArray([]);
-  this.studentDays = ko.observableArray(attendance);
 
   students.forEach(function(student) {
     self.studentList.push(new Student(student));
   });
-
 };
+var debugVM = new ViewModel();
 
-ko.applyBindings(new ViewModel());
+ko.applyBindings(debugVM);
